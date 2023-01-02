ad9320520629b1db6df689ae9307467ed6b667b0
daeyounggu <ocstar9@gmail.com>
2023-01-02 18:51:29 +0900

    Add OrderItem (OrderLine or LineItem)

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,2 +1,44 @@
-package jpabook.jpashop.domain;public class Order {
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+//    private Date date; 날짜 관련 어노테이션 매핑해야됨. Java8부터는 하비어네이트에서 LocalDateTime 지원해줌
+    private LocalDateTime orderDate; //주문시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status; //주문상태 [ORDER, CANCEL]
+
+
+
+
+
+
+
+
 }
