ff43e9dbdb39a4b3627b56ff2a20548aa9c0d5fe
daeyounggu <dae9dae0@gmail.com>
2023-04-11 04:51:20 +0900

    Add HomeController to controller package

--- src/main/java/jpabook/jpashop/controller/HomeController.java
+++ src/main/java/jpabook/jpashop/controller/HomeController.java
@@ -1,2 +1,18 @@
-package jpabook.jpashop.controller;public class HomeController {
+package jpabook.jpashop.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j // Lombok 사용 시 해당 어노트에션 사용 가능. Logger log = LoggerFactory.getLogger(getClass());와 동일한 기능을 함.
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        log.info("home controller");
+        return "home";
+    }
 }
