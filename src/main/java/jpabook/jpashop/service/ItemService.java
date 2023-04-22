0ebfd648152f91696298cc5904479cefcf773eba
daeyounggu <dae9dae0@gmail.com>
2023-04-23 01:35:47 +0900

    setter에서 변경메서드로 변경 setter의 사용은 변경지점 찾기가 어렵다. 그러므로 별도로 메서드 생성하여 응집도를 증가시켰다. 변경지점이 찾기 쉬워 질 것이다. rf. 제약으로  author, isbn 변경 불가

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -24,9 +24,7 @@ public class ItemService {
     @Transactional
     public void updateItem(Long itemId, String name, int price, int stockQuantity) {
         Item findItem = itemRepository.findOne(itemId);
-        findItem.setName(name);
-        findItem.setPrice(price);
-        findItem.setStockQuantity(stockQuantity);
+        findItem.change(name, price, stockQuantity);
     }
 
     public List<Item> findItems(){
